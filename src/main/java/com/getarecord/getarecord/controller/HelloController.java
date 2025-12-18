1c39e55727c0af2f5c65228d07e4280a5e97cee6
valentin-orrit <vorrit@gmail.com>
2025-12-18 11:06:03 +0100

    setup: basic Dockerfile + hello controller

---
+++ src/main/java/com/getarecord/getarecord/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.getarecord.getarecord.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return String.format("hello");
+    }
+}
