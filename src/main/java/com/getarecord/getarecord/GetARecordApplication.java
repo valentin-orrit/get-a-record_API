1fcb08c0bcf428ce05ba221a9231059dec55b699
valentin-orrit <vorrit@gmail.com>
2025-12-18 10:26:28 +0100

    setup

---
+++ src/main/java/com/getarecord/getarecord/GetARecordApplication.java
@@ -0,0 +1,13 @@
+package com.getarecord.getarecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetARecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetARecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getarecord/getarecord/GetARecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getarecord.getarecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetARecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
