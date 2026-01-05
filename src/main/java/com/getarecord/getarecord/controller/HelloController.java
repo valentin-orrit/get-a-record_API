f77c2ed9297414383360be5a8b83839da3a3b433
valentin-orrit <vorrit@gmail.com>
2026-01-05 14:52:49 +0100

    setup: ENV variables setup

--- src/main/java/com/getarecord/getarecord/controller/HelloController.java
+++ src/main/java/com/getarecord/getarecord/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.getarecord.getarecord.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${lastfm.api.sharedSecret}")
+    private String lastfmSharedSecret;
+
     @GetMapping("/api/hello")
     public String hello() {
-        return String.format("hello");
+        return String.format("hello ! \nlast.fm shared secret is %s.", lastfmSharedSecret);
     }
 }
