3af541754e86c2f672da2e1ab9db4a2cd7b86a58
valentin-orrit <vorrit@gmail.com>
2026-01-19 10:46:34 +0100

    lastfm: create LastFm class + controller

--- src/main/java/com/getarecord/getarecord/controller/LastfmController.java
+++ src/main/java/com/getarecord/getarecord/controller/LastfmController.java
@@ -1,7 +1,6 @@
 package com.getarecord.getarecord.controller;
 
 import com.getarecord.getarecord.lasftm.Lastfm;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
