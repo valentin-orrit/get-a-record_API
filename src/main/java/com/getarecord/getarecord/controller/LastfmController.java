11cbd67d1d4befbfe323c28132b42c8f4facb422
valentin-orrit <vorrit@gmail.com>
2026-01-20 15:09:43 +0100

    views: basic view setup to list api requests

--- src/main/java/com/getarecord/getarecord/controller/LastfmController.java
+++ src/main/java/com/getarecord/getarecord/controller/LastfmController.java
@@ -4,10 +4,12 @@ import com.getarecord.getarecord.dto.Album;
 import com.getarecord.getarecord.dto.Artist;
 import com.getarecord.getarecord.service.Lastfm;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class LastfmController {
     private final Lastfm lastfm;
 
@@ -15,12 +17,12 @@ public class LastfmController {
         this.lastfm = lastfm;
     }
 
-    @GetMapping("/api/artist")
+    @GetMapping("/artist")
     public Artist artist(@RequestParam(defaultValue = "9ddce51c-2b75-4b3e-ac8c-1db09e7c89c6") String mbid) {
         return lastfm.getArtistInfosFromMbid(mbid);
     }
 
-    @GetMapping("/api/album")
+    @GetMapping("/album")
     public Album album(@RequestParam(defaultValue = "02aa03a5-001b-4e5a-b3ad-23ad0fadb49c") String mbid) {
         return lastfm.getAlbumInfosFromMbid(mbid);
     }
